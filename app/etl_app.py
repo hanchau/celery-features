f1a7d7f6b92e20b8818b45a9be5731499537805f
Anuj Chauhan <an12ch98@gmail.com>
2020-04-15 16:49:18 +0530

    added the routing usage

--- app/etl_app.py
+++ app/etl_app.py
@@ -1,7 +1,18 @@
-# import pdb; pdb.set_trace()
+import pdb; pdb.set_trace()
 from celery import Celery
 
 etl_app = Celery('tasks', broker='amqp://hanchau:hanchau@localhost/vhost1'
 ,backend='amqp://hanchau:hanchau@localhost/vhost1'
 ,include=['workers.all_tasks', 'callbacks.all_callbacks']
 )
+
+
+# from kombu import Queue
+#
+# etl_app.conf.task_default_queue = 'default'
+# etl_app.conf.task_queues = (
+#     Queue('sum_list_only'),
+#     Queue('default'),
+# )
+#
+# etl_app.conf.task_routes = {'workers.all_tasks.sum_list': {'queue': 'sum_list_only'}}
